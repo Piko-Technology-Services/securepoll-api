c74209d42e2251b165ca9824629dc9b411984183
6upe <katongobupe444@gmail.com>
2026-03-25 11:27:27 +0200

    Task: /me

--- routes/api.php
+++ routes/api.php
@@ -65,3 +65,7 @@ Route::get('/', function () {
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->get('/me', function (Request $request) {
+    return response()->json($request->user());
+});
